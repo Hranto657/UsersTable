d5496522de3c7a2daec56f0a90ab9f6f0e01a494
Hranto <hrant.simonyan.2018@gmail.com>
2022-05-06 15:57:44 +0400

    UsersTable

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React from "react";
+import axios from "axios";
+import {
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+
+export default class App extends React.Component {
+  state = {
+    persons: [],
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+      const persons = response.data;
+      this.setState({ persons });
+    });
+  }
+  render() {
+    return (
+      <TableContainer>
+        <Table variant="simple" colorScheme="telegram">
+          <TableCaption
+            placement="top"
+            fontSize="2xl"
+            fontWeight="bold"
+            color="green.500"
+          >
+            Table for Users
+          </TableCaption>
+
+          <Thead>
+            <Tr>
+              <Th fontSize="sm" color="blue.500">
+                Full Name:
+              </Th>
+              <Th fontSize="sm" color="blue.500">
+                E-mail:
+              </Th>
+              <Th fontSize="sm" color="blue.500">
+                UserName:
+              </Th>
+              <Th fontSize="sm" color="blue.500">
+                Phone:
+              </Th>
+              <Th fontSize="sm" color="blue.500">
+                Website:
+              </Th>
+              <Th fontSize="sm" color="blue.500">
+                Address:
+              </Th>
+            </Tr>
+          </Thead>
+          {this.state.persons.map((person) => {
+            return (
+              <Tbody>
+                <Tr>
+                  <Th fontSize="12px" color="teal.400">
+                    {person.name}
+                  </Th>
+                  <Th fontSize="10px">{person.email}</Th>
+                  <Th fontSize="10px">{person.username}</Th>
+                  <Th fontSize="10px">{person.phone}</Th>
+                  <Th fontSize="10px">{person.website}</Th>
+                  <Th fontSize="10px">
+                    {person.address.city},{person.address.street}
+                    <br />
+                    {person.address.suite},ZipCode:{person.address.zipcode}
+                  </Th>
+                </Tr>
+              </Tbody>
+            );
+          })}
+        </Table>
+      </TableContainer>
+    );
+  }
+}
